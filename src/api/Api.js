bbc6c8dff871ecb463f208c111f31146d2b4f430
kot monstr <kotmonstr@ukr.net>
2020-01-29 14:14:39 +0300

    add

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,74 +1,60 @@
-import React,{ Component} from 'react';
-import Axios from 'axios';
-class Api extends Component{
+import React, {Component} from 'react';
+import axios from 'axios';
+import { Card } from 'react-bootstrap';
+
+class Api extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            // posts: [
-            //     {title: 'Статья 1', content: 'содержание статьи 1'},
-            //     {title: 'Статья 2', content: 'содержание статьи 2'},
-            //     {title: 'Статья 3', content: 'содержание статьи 3'},
-            //     {title: 'Статья 4', content: 'содержание статьи 4'},
-            //
-            //
-            // ],
-            posts : [this.getPosts]
-
+            posts: []
         };
     }
 
-
-    getPosts = () =>{
-        const result = [
-            {title: 'Статья 1', content: 'содержание статьи 1'},
-            {title: 'Статья 2', content: 'содержание статьи 2'},
-            {title: 'Статья 3', content: 'содержание статьи 3'},
-            {title: 'Статья 4', content: 'содержание статьи 4'}
-            ];
-      //  console.log('getPosts');
-
-         const x = Axios.get('http://wp.loc/wp-json/wp/v2/posts').then(function(response) {
-
-            const r = response.data;
-          //  console.log(r);
-
-            var xx = r.map((post, index) => {
-            //    console.log(post.id);
-                 result.push({ title : post.title.rendered, content : post.content.rendered})
-return result;
-            });
-            console.log(xx);
-            return result;
-        });
-
-         console.log(x);
-        return x;
+    componentDidMount() {
+        this.getPosts();
     }
 
+    getPosts = async () => {
 
+        const posts = [];
 
-    render(){
+        let queryrResult = await axios.get('http://sevgaz.loc/wp-json/wp/v2/posts').then(function (response) {
+            return response.data
+        });
+
+        queryrResult.map((post, index) => {
+            posts.push({title: post.title.rendered, content: post.content.rendered})
+        });
+
+        this.setState({
+            posts
+        });
+
+    };
+
+    render() {
+        console.log(this.state.posts);
 
         return (
-
             <div>
+                {
+                    this.state.posts.map((post, index) => {
+                        return (
+                            <Card  key={index} >
+                                <Card.Body  >
+                                    <h1>{post.title}</h1>
+                                    <p dangerouslySetInnerHTML={{ __html: post.content }} ></p>
+                                </Card.Body>
+                            </Card>
 
-                { this.state.posts.map((post,index)=>{
-                    return(
-                        <div key={index}>
-                            <div>
-                                Title: {post.title}
-                                Content: {post.content}
-                            </div>
-                        </div>
-                    )
-                })}
-
+                        )
+                    })
+                }
             </div>
         );
     }
-
 }
+
 export default Api;
